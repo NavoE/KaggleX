2223d31b04c22f11bd465fd426e2631f7638b620
NavoE <navoemmanuel@gmail.com>
2023-10-28 12:39:09 -0400

    Update

--- prompt_app.py
+++ prompt_app.py
@@ -318,7 +318,8 @@ with tab1:
               st.info(google_research)
           with st.expander("Wikipedia Research"):
               st.info(wiki_research)
-
+  else:
+      st.write("Please select the Fine-Tuned OpenAI Model setting")
 #Selects which model to run and generate on tab 2
 with tab2:
 #Creates button for generating content
@@ -355,6 +356,8 @@ with tab2:
             st.write(instagram2)
           # with st.expander("Instagram Post History"):
           #     st.info(instagram_memory.buffer)
+  else:
+    st.write("Please select the Default OpenAI Model setting")
 
 #Adds data table to tab 2
 with tab3: 
